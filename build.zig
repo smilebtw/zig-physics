9530911d1781cff4758d7d25bb94baffbf33c105
smile <smilezera@disroot.org>
2023-09-16 01:56:25 -0300

    feat: add run into build

--- build.zig
+++ build.zig
@@ -13,8 +13,10 @@ pub fn build(b: *std.Build) !void {
 
     b.installArtifact(exe);
     const run_cmd = b.addRunArtifact(exe);
-
     run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 
     raylib.addTo(b, exe, target, mode);
 }
