994aad370da8622e65f3d0d431ecc6b5b2b140f4
smile <smilezera@disroot.org>
2023-09-19 22:42:52 -0300

    feat: add scale but y axis is inverted

--- src/main.zig
+++ src/main.zig
@@ -18,9 +18,10 @@ pub fn min(a: f32, b: f32) f32 {
     return b;
 }
 
-pub fn main() void {
+pub fn main() !void {
     const window_width: i32 = 1000;
     const window_height: i32 = 1000;
+    const zoom_factor: f32 = 5.0;
 
     const Blocks = enum { SAND, DIRT, WATER, AIR };
 
@@ -30,7 +31,7 @@ pub fn main() void {
     BlocksColor.set(Blocks.DIRT, raylib.BROWN);
     BlocksColor.set(Blocks.WATER, raylib.BLUE);
 
-    var Grid: [800][800]Blocks = undefined;
+    var Grid: [1000][1000]Blocks = undefined;
 
     for (Grid, 0..) |row, y| {
         for (row, 0..) |_, x| {
@@ -44,26 +45,28 @@ pub fn main() void {
     raylib.SetConfigFlags(.{ .FLAG_WINDOW_RESIZABLE = true, .FLAG_VSYNC_HINT = true });
     raylib.SetTargetFPS(60);
 
-    var gameScreenWidth: i32 = 800;
-    var gameScreenHeight: i32 = 800;
+    var game_screen_width: i32 = 800;
+    var game_screen_height: i32 = 800;
 
-    const target: raylib.RenderTexture2D = raylib.LoadRenderTexture(gameScreenWidth, gameScreenHeight);
+    const target: raylib.RenderTexture2D = raylib.LoadRenderTexture(game_screen_width, game_screen_height);
     defer raylib.UnloadRenderTexture(target);
     raylib.SetTextureFilter(target.texture, raylib.RL_TEXTURE_FILTER_LINEAR);
 
     var isDrawing: bool = false;
     while (!raylib.WindowShouldClose()) {
-        var screenWidth: f32 = @floatFromInt(raylib.GetScreenWidth());
-        var screenHeight: f32 = @floatFromInt(raylib.GetScreenHeight());
-        var gameScreenWidth_f: f32 = @floatFromInt(gameScreenWidth);
-        var gameScreenHeight_f: f32 = @floatFromInt(gameScreenHeight);
-        var scale: f32 = min(screenWidth / gameScreenWidth_f, screenHeight / gameScreenHeight_f);
+        var screen_width: f32 = @floatFromInt(raylib.GetScreenWidth());
+        var screen_height: f32 = @floatFromInt(raylib.GetScreenHeight());
+
+        var game_screen_width_f: f32 = @floatFromInt(game_screen_width);
+        var game_screen_height_f: f32 = @floatFromInt(game_screen_height);
+
+        var scale: f32 = min(screen_width / game_screen_width_f, screen_height / game_screen_height_f) * zoom_factor;
 
         var mouse: raylib.Vector2 = raylib.GetMousePosition();
-        var virtualMouse = raylib.Vector2{ .x = 0 };
-        virtualMouse.x = (mouse.x - screenWidth - (gameScreenWidth_f * scale) * 0.5) / scale;
-        virtualMouse.y = (mouse.y - screenHeight - (gameScreenHeight_f * scale) * 0.5) / scale;
-        virtualMouse = raylib.Vector2Clamp(virtualMouse, raylib.Vector2{ .x = 0, .y = 0 }, raylib.Vector2{ .x = gameScreenWidth_f, .y = gameScreenHeight_f });
+        var virtualMouse = raylib.Vector2{ .x = 0, .y = 0 };
+        virtualMouse.x = (mouse.x - (screen_width - (game_screen_width_f * scale)) * 0.5) / scale;
+        virtualMouse.y = (mouse.y - (screen_height - (game_screen_height_f * scale)) * 0.5) / scale;
+        virtualMouse = raylib.Vector2Clamp(virtualMouse, raylib.Vector2{ .x = 0, .y = 0 }, raylib.Vector2{ .x = game_screen_width_f, .y = game_screen_height_f });
 
         if (raylib.IsMouseButtonDown(raylib.MouseButton.MOUSE_BUTTON_LEFT)) {
             isDrawing = true;
@@ -72,27 +75,29 @@ pub fn main() void {
         }
 
         raylib.BeginTextureMode(target);
-        defer raylib.EndDrawing();
         raylib.ClearBackground(raylib.BLACK);
         if (isDrawing) {
             var posX: u32 = @intFromFloat(virtualMouse.x);
             var posY: u32 = @intFromFloat(virtualMouse.y);
-
             Grid[posY][posX] = Blocks.DIRT;
         }
 
-        for (0..@intCast(gameScreenHeight)) |y| {
-            for (0..@intCast(gameScreenWidth)) |x| {
+        for (0..@intCast(game_screen_height)) |y| {
+            for (0..@intCast(game_screen_width)) |x| {
                 if (Grid[y][x] != Blocks.AIR) {
                     raylib.DrawPixel(@intCast(x), @intCast(y), BlocksColor.get(Grid[y][x]));
                 }
             }
         }
+
         raylib.EndTextureMode();
 
         raylib.BeginDrawing();
-        defer raylib.EndDrawing();
+        raylib.ClearBackground(raylib.BLACK);
 
+        var destRec: raylib.Rectangle = raylib.Rectangle{ .x = (screen_width - game_screen_width_f * scale) * 0.5, .y = (screen_height - game_screen_height_f * scale) * 0.5, .width = game_screen_width_f * scale, .height = game_screen_height_f * scale };
+
+        raylib.DrawTexturePro(target.texture, raylib.Rectangle{ .x = 0, .y = 0, .width = game_screen_width_f, .height = game_screen_height_f }, destRec, raylib.Vector2{ .x = 0, .y = 0 }, 0.0, raylib.WHITE);
         raylib.EndDrawing();
     }
 
